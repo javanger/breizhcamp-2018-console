92741b207ead3024217b827a5ee083d54d0c0aca
Axel Buteau <buteauaxel@gmail.com>
2018-04-12 17:40:41 +0200

    axel lister presentateurs

--- ihm.js
+++ ihm.js
@@ -1,44 +1,56 @@
-var service = require('./service');
-var readline = require('readline');
+var service = require("./service");
+var readline = require("readline");
 
 var rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
+  input: process.stdin,
+  output: process.stdout
 });
 
-
-console.log('** Application BreizhCamp 2018 **');
+console.log("** Application BreizhCamp 2018 **");
 
 exports.start = function() {
-    demarrer();
-}
-function demarrer(){
+  demarrer();
+};
+function demarrer() {
+  rl.question(Menu(), function(saisie) {
+    switch (saisie) {
+      case "1":
+        service.init(function(nb) {
+          console.log("\n" + nb + " Données mises à jour \n");
+          demarrer();
+        });
+        break;
 
-rl.question(Menu(), function(saisie){
-    switch(saisie){
-    case '1' :
-    service.init(function(nb) {
-        console.log("\n" +nb + ' Données mises à jour \n');  
-         demarrer();
-        });        
-           break;
-           
-    case '2':  
-       service.listerSessions(function(chaine) {
-        console.log(chaine);
-    demarrer(); 
-       }); 
-          
-            break;
-    case '99' :
-        console.log('Au revoir');
+      case "2":
+        service.listerSessions(function(chaine) {
+          console.log(chaine);
+          demarrer();
+        });
+
+        break;
+      case "3":
+      service.listerPresentateurs(function(groot) {
+          groot.forEach(function (g){
+            console.log(g.name);
+          });
+        
+        demarrer();
+      });
+
+        break;
+      case "99":
+        console.log("Au revoir");
         rl.close();
         break;
     }
-});
-};
+  });
+}
 
-function Menu(){
-    var chaine = '************************* ' + '\n 1. Rafraichir les données'+ '\n 2. Lister les sessions'+ '\n 99. Quitter\n'
-    return chaine;
+function Menu() {
+  var chaine = "************************* ";
+  chaine += "\n 1. Rafraichir les données";
+  chaine += "\n 2. Lister les sessions";
+  chaine += "\n 3. Lister les présentateurs";
+  chaine += "\n 99. Quitter \n";
+  return chaine;
 }
--- service.js
+++ service.js
@@ -1,31 +1,54 @@
 var request = require("request");
+var jsdom = require("jsdom");
 // tableau qui contiendra toutes les sessions du BreizhCamp
 
 var talks = [];
+var groot = [];
+
 var URL_API = "http://www.breizhcamp.org/json/talks.json";
 var URL_OTHER = "http://www.breizhcamp.org/json/others.json";
+var URL_P = "http://www.breizhcamp.org/conference/speakers/";
 exports.init = function(callback) {
   request(URL_API, { json: true }, function(err, res, body) {
-    request(URL_OTHER, { json: true }, function(err, res, body){
-    if (err) {
-      return console.log("Erreur", err); }
+    request(URL_OTHER, { json: true }, function(err, res, body) {
+      if (err) {
+        return console.log("Erreur", err);
+      }
       talks = talks.concat(body);
       callback(talks.length);
     });
     if (err) {
-        return console.log("Erreur", err); }
+      return console.log("Erreur", err);
+    }
     talks = body;
-  ;
- });
+
+    request(URL_P, {}, function(err, res, body) {
+      if (err) {
+        return console.log("Erreur", err);
+      }
+      var dom = new jsdom.JSDOM(body);
+      var nm = dom.window.document.querySelectorAll(".speaker .media-heading");
+      
+      nm.forEach(function(lg) {
+       
+
+        groot.push({
+          'name': lg.innerHTML
+        });
+        
+      });
+    });
+  });
 };
 
-exports.listerSessions = function (callback) {
-     var t = "";
-    talks.forEach(function(talk) {
-    t = t.concat(talk.name+ " ( "+ talk.speakers + " )\n");
-        });
-        callback(t);
-      };
+exports.listerSessions = function(callback) {
+  var t = "";
+  talks.forEach(function(talk) {
+    t = t.concat(talk.name + " ( " + talk.speakers + " )\n");
+  });
+  callback(t);
+};
 
-
-
+exports.listerPresentateurs = function(callback) {
+  callback(groot);
+};
